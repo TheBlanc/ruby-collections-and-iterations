beaad54e90d1ceac2b9994cf3bfbecca28f34a6d
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-05-24 16:44:19 -0400

    completed exercise 1

--- exercises.rb
+++ exercises.rb
@@ -1,5 +1,44 @@
+# Exercise 0
 fav_colors = ["red", "blue", "yellow", "orange"]
 ages = [29, 32, 33, 65, 27]
 heads_tails = ["heads", "tails", "heads", "heads", "tails"]
 live_music = ["Ander Paak", "Daniel Ceaser", "Jamie XX"]
 fav_colors2 = [:red, :blue, :yellow, :orange]
+
+dictionary = {
+  :toronto => "The 6ix",
+  :bike => "Mode of transportation",
+  :computer => "The thing you are looking at"
+}
+movie_dates = {
+  :jaws => 1975,
+  :wreck_it_ralph => 2012,
+  :gone_Girl => 2014
+}
+city_pop = {
+  :toronto => 5928040,
+  :nyc => 8538000,
+  :la => 3976000
+}
+sibling_age = {
+  :meg => 32,
+  :adam => 29,
+  :hamed => 28
+}
+
+# Exercise 1
+
+# Q1
+puts heads_tails
+
+# Q2
+puts fav_colors[0]
+
+# Q3
+puts ages.sort
+
+# Q4
+ages.push(0)
+
+# Q5
+puts movie_dates[:wreck_it_ralph]
